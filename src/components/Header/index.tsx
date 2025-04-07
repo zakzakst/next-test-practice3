4aec32518d5569a3186fcfa4caba1a23aead52de
zakzakst <e1433104@gmai.com>
2025-04-07 12:46:01 +0900

    修正

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,2 +1,19 @@
 // TODO: 下記らへん作る
 // https://github.com/frontend-testing-book/nextjs/tree/main/src/components/layouts/BasicLayout/Header
+import { LinkButton } from "@/components/atoms/LinkButton";
+import { useUser } from "@/components/providers/User/UserProvider";
+
+export const Header = () => {
+  const { user } = useUser();
+  return (
+    <header>
+      {user ? (
+        <p>
+          {user.id}: {user.name}
+        </p>
+      ) : (
+        <p>ユーザー情報がありません</p>
+      )}
+    </header>
+  );
+};
