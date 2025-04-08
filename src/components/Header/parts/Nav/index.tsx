89d7edfb3ecbdaecd6aecc762b570b77bfb83578
zakzakst <e1433104@gmai.com>
2025-04-09 08:25:57 +0900

    修正

---
+++ src/components/Header/parts/Nav/index.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { AnchorHTMLAttributes } from "react";
+import styles from "./styles.module.css";
+
+const isCurrent = (flag: boolean): AnchorHTMLAttributes<HTMLAnchorElement> => {
+  if (!flag) return {};
+  return { "aria-current": "page" };
+};
+
+type Props = {
+  onCloseMenu: () => void;
+};
+
+export const Nav = ({ onCloseMenu }: Props) => {
+  const { pathname } = useRouter();
+  return (
+    <nav aria-label="ナビゲーション" className={styles.nav}>
+      <button
+        aria-label="メニューを閉じる"
+        className={styles.closeMenu}
+        onClick={onCloseMenu}
+      />
+      <ul className={styles.list}>
+        <li>
+          <Link
+            href="/my/posts"
+            {...isCurrent(
+              pathname.startsWith("/my/posts") &&
+                pathname !== "/my/posts/create"
+            )}
+          >
+            My Posts
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/my/posts/create"
+            {...isCurrent(pathname === "/my/posts/create")}
+          >
+            Create Post
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
